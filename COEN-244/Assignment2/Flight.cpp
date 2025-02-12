816599c2027c49a5e455fabfbd7997bdb66fbd1b
Tromboman22 <109414093+Tromboman22@users.noreply.github.com>
2025-02-12 14:28:12 -0500

    Update Flight.cpp

--- COEN-244/Assignment2/Flight.cpp
+++ COEN-244/Assignment2/Flight.cpp
@@ -22,31 +22,32 @@ Flight::Flight(const Flight &)  //copy constructor
 }; 
 string Flight:: getID()
 {
-
+    return id;
 };
 string Flight:: getDepCity()
 {
-
+    return departureCity;
 };
 string Flight:: getArrCity()
 {
-
+    return arrivalCity;
 };
 Date Flight:: getDepDate()
 {
-
+    return departureDate;
 };
 Date Flight:: getArrDate()
 {
-
+    return arrivalDate;
 };
 int Flight:: getDuration()
 {
-
+    return duration;
 };
 void Flight:: printinfo()
 {
-
+    cout << "ID: " << id << endl;
+    //print the rest of the data members
 };
 Flight::~Flight()   //Destructor
 {
