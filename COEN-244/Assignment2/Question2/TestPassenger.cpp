57d30cee080a68f48367ef1494bc38bb496c2bcc
Tromboman22 <109414093+Tromboman22@users.noreply.github.com>
2025-02-13 18:35:55 -0500

    Update TestPassenger.cpp

--- COEN-244/Assignment2/Question2/TestPassenger.cpp
+++ COEN-244/Assignment2/Question2/TestPassenger.cpp
@@ -10,5 +10,23 @@ using namespace std;
 
 int main()
 {
+    //simulate new passenger
+
+    string id, firstName, lastName, address, email;
+
+    cout << "Choose your passenger id: ";
+    cin >> id;
+    cout << "Enter your first name: ";
+    cin >> firstName;
+    cout << "Enter your last name: ";
+    cin >> lastName;
+    cin.clear();
+    cout << "Enter your address : ";
+    getline(cin, address);
+    cout << "Enter your email: ";
+    cin >> email;
+    Passenger p = Passenger(id, firstName, lastName, address, email);
+    p.Print();
+
     return 0;
 }
