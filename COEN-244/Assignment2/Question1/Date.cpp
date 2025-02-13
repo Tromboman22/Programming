9d007684c70c2519d6812aa6b8aaac3b955dde6a
Tromboman22 <109414093+Tromboman22@users.noreply.github.com>
2025-02-13 18:29:36 -0500

    Update Date.cpp

--- COEN-244/Assignment2/Question1/Date.cpp
+++ COEN-244/Assignment2/Question1/Date.cpp
@@ -13,19 +13,13 @@ Date::Date(int d, int m, int y, int hour, int min, int sec) {
     cout << "Constructor Date(...) is called" << endl;
     day = d;
     month = m;
-    year = y ;
-    time.setTime(hour, min, sec);
+    year = y;
+    time = new Time();
+    time->setTime(hour, min, sec);
 }
 
 void Date::printDate() const {
-   cout << month << "/" << day << "/" << year << " ";
-   time.printTime() ;
+   cout << month << "/" << day << "/" << year << " at ";
+   time->printTime() ;
 }
 
-
-int main() 
-{
-    Date date(10, 12, 2006, 10, 20, 45);
-    cout << "The date is:" << endl;
-    date.printDate();
-}
